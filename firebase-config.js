f616343b70de92817383f9ca12feee02cb8e7cf4
uchralbymbaa538 <uchralbymbaa538@gmail.com>
2026-07-17 02:42:24 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// -----------------------------------------------------------------------
+// ЭНЭ ФАЙЛЫГ GITHUB-Т ХЭЗЭЭ Ч PUSH ХИЙЖ БОЛОХГҮЙ!
+// .gitignore файлд "firebase-config.js" гэж заавал нэмсэн байх ёстой.
+// -----------------------------------------------------------------------
+export const firebaseConfig = {
+  apiKey: "AIzaSyDmhjLV2oRFnvM3pkT9P3YqYfylQWDM7WY",
+  authDomain: "aduu-yamaanii-jin.firebaseapp.com",
+  projectId: "aduu-yamaanii-jin",
+  storageBucket: "aduu-yamaanii-jin.firebasestorage.app",
+  messagingSenderId: "693580355410",
+  appId: "1:693580355410:web:2a10c3ad16f91b290f0c8b",
+  measurementId: "G-SS01WFS2WP"
+};
